8b3f3393993dac74ba59ecd5a4e13682360c2a51
Ilya Belyaev <60135683+BelyaevIlyaUkr@users.noreply.github.com>
2020-05-31 11:56:22 +0300

    Error handling check left

--- LexerWithParserWithCodeGenerator/Code_Generator.cpp
+++ LexerWithParserWithCodeGenerator/Code_Generator.cpp
@@ -7,18 +7,40 @@
     else
         error_collect(&errorMessages,error_type,currentLexeme,expectedLexeme);
 }*/
-bool identifier_identity(string identifier_value,string*firstUsedIdentifier){
+bool identifier_identity(int* state,string identifier_value,string*firstUsedIdentifier,string identifiers_type,string*few_identifiers_declaration){
     static vector<string>used_identifiers_value;
+    static int one_type_identifiers_count = 0;
     if (firstUsedIdentifier != NULL){
         *firstUsedIdentifier =used_identifiers_value[0];
         return false;
     }
-    for(int i = 0; i < used_identifiers_value.size();i++){
-        if(used_identifiers_value[i] == identifier_value)
-            return true;
+    else if (identifiers_type != ""){
+        for(int i = 0; i < one_type_identifiers_count; i++){
+            if (used_identifiers_value.size() > 2){
+                *few_identifiers_declaration = *few_identifiers_declaration + ", ";
+            }
+            else{
+                *few_identifiers_declaration = *few_identifiers_declaration + "\n\t";
+            }
+
+            *few_identifiers_declaration = *few_identifiers_declaration + 
+            used_identifiers_value[used_identifiers_value.size() - one_type_identifiers_count + i] + " : " + 
+            identifiers_type;
+        }
+
+        one_type_identifiers_count = 0;
+        return false;
     }
-    used_identifiers_value.push_back(identifier_value);
-    return false;
+    else{
+        for(int i = 0; i < used_identifiers_value.size();i++){
+            if(used_identifiers_value[i] == identifier_value)
+                return true;
+        }
+        used_identifiers_value.push_back(identifier_value);
+        if(*state != 4)
+            one_type_identifiers_count++;
+        return false;
+    }
 }
 string tree_travel(struct node* syntax_tree,map<string,int>* Identifiers,int* state){
     string asm_code,asm_code_below;
@@ -43,13 +65,13 @@ string tree_travel(struct node* syntax_tree,map<string,int>* Identifiers,int* st
         && *state == 4)){
             string procedure_name;
             *state = 0;
-            identifier_identity("0",&procedure_name);
-            asm_code = "endp\t" + procedure_name + "\ncode ends\n\tend\t" + procedure_name;
+            identifier_identity(NULL,"0",&procedure_name);
+            asm_code = "\nendp\t" + procedure_name + "\ncode ends\n\tend\t" + procedure_name;
         }
 
         auto itr = Identifiers->find(currentLexeme["lexemValue"]);
         if (itr != Identifiers->end()){
-            if(identifier_identity(currentLexeme["lexemValue"])){
+            if(identifier_identity(state,currentLexeme["lexemValue"])){
                 *state = 1; //means that code execution have to be ended after finding error
             }
             if (*state == 2){
@@ -57,7 +79,7 @@ string tree_travel(struct node* syntax_tree,map<string,int>* Identifiers,int* st
                 return currentLexeme["lexemValue"]+ ":\n\tnop\ncode ends\n\tend\t" + currentLexeme["lexemValue"];
             }
             if(*state == 4){ //finding procedure identifier
-                return "proc\t" + currentLexeme["lexemValue"] + "\n";
+                return "proc\t" + currentLexeme["lexemValue"];
             }
             return currentLexeme["lexemValue"]; // returning identifiers
         }
@@ -95,7 +117,7 @@ string tree_travel(struct node* syntax_tree,map<string,int>* Identifiers,int* st
             }
             else if (asm_code_below == "FLOAT" || asm_code_below == "BLOCKFLOAT" || asm_code_below == "INTEGER"){
                 for (int i =0; i < declaration_attributes.size(); i++){
-                    if (declaration_attributes[i] == asm_code_below){
+                    if (declaration_attributes[i] == "FLOAT" || declaration_attributes[i] == "BLOCKFLOAT" || declaration_attributes[i] == "INTEGER"){
                         *state = 1;
                         break;
                         //TODO error processing
@@ -107,13 +129,18 @@ string tree_travel(struct node* syntax_tree,map<string,int>* Identifiers,int* st
                     }
                 }
                 if (*state != 1){
-                    declaration_attributes.push_back(asm_code_below);
-                    asm_code = asm_code + asm_code_below = "\tDD\n";
+                    declaration_attributes.clear();
+                    asm_code_below = "";
+                    identifier_identity(NULL,"0",NULL,"DD",&asm_code_below);
+                    asm_code = asm_code + asm_code_below;
                 }
                 else break;
             }
-            else { //identifier or nonterminal was found 
-                asm_code = asm_code + asm_code_below;
+            else { //nonterminal was found 
+                auto itr = Identifiers->find(asm_code_below);
+                if (itr == Identifiers->end()){
+                    asm_code = asm_code + asm_code_below;
+                }
                 if (*state == 3 || *state == 1) //or program identifier was found either there are few equal identifiers
                     break;
             }
--- LexerWithParserWithCodeGenerator/Code_Generator.h
+++ LexerWithParserWithCodeGenerator/Code_Generator.h
@@ -11,7 +11,7 @@ using namespace std;
 //#include "Front.h"
 #include "Parser.h"
 
-bool identifier_identity(string identifier_value,string*firstUsedIdentifier = NULL);
+bool identifier_identity(int* state,string identifier_value,string*firstUsedIdentifier = NULL,string identifiers_type="",string*few_identifiers_declaration=NULL);
 string tree_travel(struct node* syntax_tree,map<string,int>* Identifiers,int* state);
 void Code_Generator(struct node* syntax_tree,map<string,int>* Identifiers,FILE*f2);
 
